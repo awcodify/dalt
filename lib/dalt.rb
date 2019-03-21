a72f03180330baae2fa5c872ceb434019c66cf8b
Danu <awcodify@gmail.com>
2019-03-21 15:53:16 +0700

    add smart_change in Dalt module

--- lib/dalt.rb
+++ lib/dalt.rb
@@ -1,11 +1,16 @@
 require 'date'
 require 'dalt/error/without_alternate'
+require 'dalt/formatter'
 require 'dalt/parser'
 require 'dalt/version'
 
 # Dalt will use instance of Dalt::Parser
 module Dalt
-  def self.parse(text, alt: nil)
-    @date = Dalt::Parser.new(text, alt: alt).parse
+  def self.parse(date, alt: nil)
+    @date = Dalt::Parser.new(date.to_s, alt: alt).parse
+  end
+
+  def self.smart_change(date, options)
+    Dalt::Formatter.new(date.to_s, options).smart_change
   end
 end
